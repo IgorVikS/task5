2317e55804e7404f4d9f904abc71d6932a2e53dd
Игорь <temruk_siv@mail.ru>
2024-09-11 23:59:34 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,4 +1,4 @@
 name = 'Python'
-kol_compl_dz = '12'
-kol_zatrach_chas = '1.5'
-print('Курс:',name,', всего задач:',kol_compl_dz,', затрачено часов:',kol_zatrach_chas,', среднее время выполнения',float(1.5) / 12, 'часа')
+kol_dz = 12
+kol_chas = 1.5
+print('Курс:',name,', всего задач:',kol_dz,', затрачено часов:',kol_chas,', среднее время выполнения',float(kol_chas) / kol_dz, 'часа')
