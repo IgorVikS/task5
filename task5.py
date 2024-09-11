d502959cdee1d054a78d9ed66489233ae887ae60
Игорь <temruk_siv@mail.ru>
2024-09-11 22:04:00 +0300

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,4 @@
+name = 'Python'
+kol_compl_dz = '12'
+kol_zatrach_chas = '1.5'
+print('Курс:',name,', всего задач:',kol_compl_dz,', затрачено часов:',kol_zatrach_chas,', среднее время выполнения',float(1.5) / 12, 'часа')
